17d2eddf3d1afdd964c514de09d722daa66dd9d6
Andrew Rowson <github@growse.com>
2012-11-18 21:15:25 +0000

    Separated out Url cache

--- FlickrGettr.py
+++ FlickrGettr.py
@@ -8,6 +8,7 @@ import os
 import argparse
 import flickrapi
 import urllib
+import json
 
 
 class FlickrGettr:
@@ -17,6 +18,7 @@ class FlickrGettr:
         self.log = logging.getLogger('FlickrGettr')
         self.tokenfile = ".flickrtoken"
         self.path = []
+        self.pathsdb = "gettr.db"
 
     def main(self, directory, dryrun):
         log.info("Entering main")
@@ -27,6 +29,8 @@ class FlickrGettr:
         self.getFlickrClient()
         log.info("Getting existing photosets")
         self.populatePhotosets()
+        self.loadSeen()
+        log.info("{} cached urls loaded".format(len(self.flickrUrlCache)))
         for photoset in self.sets:
             log.info("Set: %s" % photoset)
             photos = self.flickr.photosets_getPhotos(photoset_id=self.sets[photoset])
@@ -45,13 +49,16 @@ class FlickrGettr:
                 secret = photoinfo.attrib.get('originalsecret')
                 extension = photoinfo.attrib.get('originalformat')
                 url = "http://farm{farmid}.staticflickr.com/{serverid}/{id}_{osecret}_o.{format}".format(farmid=farmid, serverid=serverid, id=photoid, osecret=secret, format=extension)
-                self.photosToDownload[filename] = url
+                self.photosToDownload[photoid] = filename
+                if photoid not in self.flickrUrlCache:
+                    self.flickrUrlCache[photoid] = url
+                    self.saveSeen()
                 log.info("Photo: %s is %s" % (filename, url))
 
         log.info("Complete. %s photos to download" % len(self.photosToDownload))
-        for filename in self.photosToDownload:
-            log.info("Downloading {}".format(self.photosToDownload[filename]))
-            urllib.urlretrieve(self.photosToDownload[filename], filename)
+        for photoid in self.photosToDownload:
+            log.info("Downloading {}".format(self.flickrUrlCache[photoid]))
+            urllib.urlretrieve(self.flickrUrlCache[photoid], self.photosToDownload[photoid])
 
     def createFilename(self, photoset, title):
         if title.endswith('.jpg'):
@@ -105,6 +112,20 @@ class FlickrGettr:
     def setFlickrToken(self, token):
         f = open(self.tokenfile, 'w')
         f.write(token)
+        f.close()
+
+    def loadSeen(self):
+        if not os.path.exists(self.pathsdb):
+            self.paths = []
+            return
+        f = open(self.pathsdb, 'r')
+        self.flickrUrlCache = json.load(f)
+        f.close()
+
+    def saveSeen(self):
+        log.info("Saving seen paths")
+        f = open(self.pathsdb, 'w')
+        json.dump(self.flickrUrlCache, f)
         f.close()
 
 if __name__ == '__main__':
